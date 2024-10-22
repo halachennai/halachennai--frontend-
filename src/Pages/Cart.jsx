9937064d6e1444a8fb1359fe9e564570518f73c4
Hala Chennai <halachennai527@gmail.com>
2024-10-22 17:52:26 +0530

    this is seprate frontend

---
+++ src/Pages/Cart.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CartItems from "../Components/CartItems/CartItems";
+import DeliveryNote from "../Components/DeliveryNote/DeliveryNote";
+
+const Cart = () => {
+  return (
+    <div>
+      <CartItems />
+      {/* <DeliveryNote /> */}
+    </div>
+  );
+};
+
+export default Cart;
