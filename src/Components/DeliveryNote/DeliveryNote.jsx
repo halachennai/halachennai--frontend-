93e0a23eb36145756175472b031ea10185168ccb
CowTheGreat <cowara987@gmail.com>
2024-10-28 19:16:41 +0530

    delivery changed from weeks to days

--- src/Components/DeliveryNote/DeliveryNote.jsx
+++ src/Components/DeliveryNote/DeliveryNote.jsx
@@ -6,7 +6,7 @@ const DeliveryNote = () => {
     <div className="delivery-note-container">
       <h2>Note:</h2>
       <p>
-        Delivery is expected within two weeks, but we aim to deliver sooner.
+        Delivery is expected within 14-21 days, but we aim to deliver sooner.
         Pre-order now to guarantee availability, and we'll ensure it reaches you
         as quickly as possible!
       </p>
