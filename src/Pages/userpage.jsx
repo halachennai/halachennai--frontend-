f3731008828ad4ed67e9fb92468b9333da40f07d
CowTheGreat <cowara987@gmail.com>
2024-10-31 15:58:14 +0530

    user page issue resolved

--- src/Pages/userpage.jsx
+++ src/Pages/userpage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "./CSS/Userpage.css";
+import "./CSS/userpage.css";
 
 const UserPage = () => {
   const [userData, setUserData] = useState({});
